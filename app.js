19a8e77b141f538c92ad835529465a35ef4f92fa
vagrant <vagrant@vagrant.vm>
2018-03-29 15:55:59 +0000

    database reset created

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ const dataBase = require('./database');
 const command = process.argv[2];
 console.log('Command: ', command);
 
-dataBase.connectToDataBase();
+if (command === 'resetDatabase'){
+    dataBase.resetDatabase();
+}
 
-
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello World')
-});
-
-app.listen(3000); //Port listening
+console.log('end');
+//
+// var express = require('express');
+// var app = express();
+//
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// });
+//
+// app.listen(3000); //Port listening
--- database.js
+++ database.js
@@ -1,18 +1,61 @@
 const mysql = require('mysql');
 
+const databaseName = 'node_server';
+
 const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "root"
 });
 
-module.exports.connectToDataBase = () => {
-    con.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-        con.query("CREATE DATABASE node_server", function (err, result) {
-            if (err) throw err;
-            console.log("Database created");
-        });
+module.exports.resetDatabase = () => {
+    deleteDataBase(databaseName);
+    createDataBase(databaseName);
+    createTables(databaseName);
+};
+
+const createDataBase = (databaseName) => {
+    const sql = "CREATE DATABASE " + databaseName;
+    queryPromise(sql).then(function () {
+        console.log("Database created");
+    }).catch(function (error) {
+        console.log(error.message);
     });
+};
+
+const deleteDataBase = (databaseName) => {
+    const sql = "DROP DATABASE " + databaseName;
+    queryPromise(sql).then(function () {
+        console.log("Database deleted");
+    }).catch(function (error) {
+        console.log(error.message);
+    });
+};
+
+const createTables = (databaseName) => {
+    const sql = "CREATE TABLE " + databaseName +".customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+    queryPromise(sql).then(function () {
+        console.log("Table created");
+    }).catch(function (error) {
+        console.log(error.message);
+    });
+};
+
+const query = (sql) => {
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        return result;
+    });
+};
+const queryPromise = function (sql) {
+    return new Promise(
+        function (resolve, reject) {
+            con.query(sql, function (err, result) {
+                if (err) {
+                    reject(err);
+                }
+                resolve(result);
+            });
+        }
+    )
 };
